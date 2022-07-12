0a8eef4a9d665f407cbe9f6d53a165672bae8562
Lukas Moessler <lukas.moessler@infranet.bz.it>
2022-07-12 13:59:56 +0200

    fixed bootstrap color and var overwriting

--- resources/views/partials/container5.blade.php
+++ resources/views/partials/container5.blade.php
@@ -1,31 +1,6 @@
 <section class="row align-items-center m-0 home">
     <div class="p-4 rounded m-auto">
         <h1 class="text-uppercase text-center mb-5">{{ __('container5.title') }}</h1>
-        {{-- <div class="row row-cols-xl-3 row-cols-md-2 row-cols-1 g-5 mb-5">
-
-            @forelse ($news as $article)
-                <div class="col">
-                    <a href="#" class="text-decoration-none">
-                        <div class="clearfix bg-light rounded p-3 d-flex align-items-center justify-content-between">
-                            <img src="/storage/{{ $article->banner }}" class="col-3 float-md-start m-3" alt="...">
-                            <div class="flex-column news">
-                                <h4>{{ $article->{'title_' . app()->getLocale()} }}</h4>
-                                <p class="lh-lg">{{ $article->{'content_' . app()->getLocale()} }}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div>
-                                        {{ $article->created_at->format('d.m.Y H:i') }}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-            @empty
-                {{ __('container5.no articles') }}
-            @endforelse
-
-        </div> --}}
 
         <div class="row g-md-3 g-0 my-lg-3 my-md-2 my-1">
 
